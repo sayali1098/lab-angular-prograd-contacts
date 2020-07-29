d2d2b86bc1715baa27511178b68a37427de60794
sayali1098 <sauamritkar@gmail.com>
2020-07-29 21:37:41 +0530

    FSD0620B102

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,27 +1,24 @@
-import { contacts } from './../contact';
-import { userContact } from './../userContact';
-import { Component, OnInit } from '@angular/core';
-
+import { contacts } from "./../contact";
+import { userContact } from "./../userContact";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-contact-list',
-  templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  selector: "app-contact-list",
+  templateUrl: "./contact-list.component.html",
+  styleUrls: ["./contact-list.component.css"],
 })
 export class ContactListComponent implements OnInit {
   contacts: userContact[];
   newContact: any = {};
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  // initialize your contacts here
+    this.contacts = contacts;
   }
 
   addContact(newContact): any {
-    console.log('Add contact has been called');
-     // Fill your code here
-    // add contact to contacts list
-    // clear inputs
-   
+    console.log("Add contact has been called");
+    console.log(newContact);
+    this.contacts.push(newContact);
   }
 }
